5014b03f782252bbaf9accf1ba41da6feb7ccb00
Nicolas Clementi <clementinicolas@gmail.com>
2022-12-27 17:46:58 -0300

    conectando con el back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -70,6 +71,7 @@ import { AutocompleteActoresComponent } from './actores/autocomplete-actores/aut
     BrowserAnimationsModule,
     MaterialModule,
     ReactiveFormsModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
